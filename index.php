44b23fb244f792e5154fa48f980d5df973c330f9
Rémy <remyc59440@hotmail.com>
2023-01-17 09:36:24 +0100

    da

--- index.php
+++ index.php
@@ -9,9 +9,21 @@
  *    longueur de la chaîne de la troisième constante ( attention aux priorités )
  */
 // TODO Votre code ici.
+const CONSTANTE1 = 12;
+const CONSTANTE2 = 2;
+const CONSTANTE3 = "C'est cool PHP <br>";
 
-
+echo CONSTANTE1 * CONSTANTE2;
+$result = (CONSTANTE1 + CONSTANTE2) * strlen(CONSTANTE3) . "<br>";
+echo $result;
 /**
+ *
  * 6. Utilisez la bonne constante magique et éventuellement la bonne fonction pour afficher un maximum d'informations sur la page actuelle4
  */
-// TODO Votre code ici.
+// TODO Votre code ici.²
+echo "Répértoire du fichier:" . __FILE__ . "<br>";
+echo "Répértoire :" . __DIR__  . "<br>";
+echo "Ligne numéro :" . __LINE__ . "<br>";
+echo "Ligne numéro :" . __METHOD__  . "<br>";
+echo "Ligne numéro :" . __NAMESPACE__  . "<br>";
+echo "Ligne numéro :" . __TRAIT__  . "<br>";
